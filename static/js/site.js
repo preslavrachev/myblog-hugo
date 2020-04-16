b21880f2fd2de781f2332b1c12d52b0b3949de88
Preslav Rachev <rachev.preslav@gmail.com>
2020-04-16 06:33:59 +0200

    Add HotJar analytics

--- static/js/site.js
+++ static/js/site.js
@@ -20,3 +20,18 @@ window.cookieconsent.initialise({
   },
   position: "bottom-right"
 });
+
+/* Hotjar Tracking Code for https://preslav.me */
+(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 1770517, hjsv: 6 };
+  a = o.getElementsByTagName("head")[0];
+  r = o.createElement("script");
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
